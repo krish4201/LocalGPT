961a0981f0cf63225e185e74a334cf1adc7d1151
Krishnakumar <108615733+krish4201@users.noreply.github.com>
2025-08-23 01:28:56 +0530

    Update cloning.py

--- cloning.py
+++ cloning.py
@@ -15,7 +15,7 @@ from git import Repo
 # URL of the Hugging Face repository
 repo_url = "https://huggingface.co/openai/gpt-oss-120b"
 # Local path where you want to clone the repo
-local_path = "./"
+local_path = "./Model"
 
 # Clone the repository
 Repo.clone_from(repo_url, local_path)
